33952e52692f369450d3cd619e363a960c3da906
gavin fournier <gavinf2@learn.cssd.ab.ca>
2019-02-11 13:45:19 -0700

    designed and began programming the app

--- magic 8ball/ViewController.swift
+++ magic 8ball/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomBallIndex : Int = 0
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
+    @IBOutlet weak var eightballImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+    
+    randomBallIndex = Int(arc4random_uniform(5))
+        
+        print(randomBallIndex)
+        
+        eightballImageView1.image = UIImage(named: ballArray[1])
+    
+    }
+    
 }
 
