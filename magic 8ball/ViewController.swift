8a9e5cd0dea359733273570a6d40f87a5de27bb5
gavin fournier <gavinf2@learn.cssd.ab.ca>
2019-02-11 13:21:06 -0700

    Initial Commit

---
+++ magic 8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic 8ball
+//
+//  Created by Gavin Fournier on 2019-02-11.
+//  Copyright © 2019 Gavin Fournier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
